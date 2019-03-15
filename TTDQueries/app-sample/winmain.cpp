518d83b5dff89b6b4cf753314e2147ba15cf987b
Ivette Carreras <ivcarr@microsoft.com>
2019-03-15 12:24:46 -0700

    Add a sample program to run queries against
    Taking a sample movie player from the Windows 7 SDK samples. The sample is modified to show a specific error when loading a bad file.

---
+++ TTDQueries/app-sample/winmain.cpp
@@ -0,0 +1,99 @@
+//////////////////////////////////////////////////////////////////////////
+// winmain.cpp : Defines the entry point for the application.
+// 
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include "DShowPlayer.h"
+#include "resource.h"
+
+#include "MainWindow.h"
+
+#define MAX_LOADSTRING 100
+
+WCHAR g_commandLine[2048];
+
+
+int APIENTRY wWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPWSTR     lpCmdLine,
+                     int       nCmdShow)	
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+
+    wcscpy_s(g_commandLine, lpCmdLine);
+
+	MSG msg;
+	HACCEL hAccelTable;
+    HRESULT hr = S_OK;
+
+
+	INITCOMMONCONTROLSEX icc;
+	icc.dwSize = sizeof(icc);
+	icc.dwICC = ICC_COOL_CLASSES | ICC_BAR_CLASSES;
+	
+    if (!InitCommonControlsEx(&icc))
+    {
+        NotifyError(NULL, TEXT("InitCommonControlsEx failed."), E_FAIL);
+        return -1;
+    }
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DSHOWPLAYER));
+
+	
+	hr = CoInitialize(NULL);
+    if (FAILED(hr))
+    {
+        NotifyError(NULL, TEXT("CoInitialize failed."), hr);
+        return -1;
+    }
+
+	MainWindow *pWin = new MainWindow();
+
+    if (pWin == NULL)
+    {
+        hr = E_OUTOFMEMORY;
+    }
+
+    if (SUCCEEDED(hr))
+    {
+    	hr = pWin->Create(hInstance);
+    }
+
+    if (SUCCEEDED(hr))
+    {
+    	hr = pWin->Show(nCmdShow);
+    }
+
+    if (FAILED(hr))
+    {
+        NotifyError(NULL, TEXT("Could not initialize the application"), hr);
+        CoUninitialize();
+        return -1;
+    }
+
+
+	// Main message loop:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	delete pWin;
+
+	CoUninitialize();
+
+	return (int) msg.wParam;
+}
+
+
